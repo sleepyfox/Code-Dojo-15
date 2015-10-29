e0c93d80cb76df5abb3afac6ab7c24b438c241bd
Fox <sleepyfox@gmail.com>
2015-10-29 14:24:29 +0000

    [refactor] move update sell in to Stock_Item class

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-ITEM_AGED_BRIE = "Aged Brie"
-ITEM_BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
-ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros"
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 class GildedRose(object):
 
@@ -11,8 +11,8 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            item._update_sell_in_value()
             self._update_quality_value(item)
-            self._update_sell_in_value(item)
             self._update_quality_when_sellin_passed(item)
 
     def _improve_quality(self, item):
@@ -24,26 +24,22 @@ class GildedRose(object):
             item.quality -= 1
 
     def _update_quality_value(self, item):
-        if item.name == ITEM_AGED_BRIE or item.name == ITEM_BACKSTAGE_PASS:
+        if item.name == AGED_BRIE or item.name == BACKSTAGE_PASS:
             self._improve_quality(item)
-            if item.name == ITEM_BACKSTAGE_PASS:
-                if item.sell_in < 11:
+            if item.name == BACKSTAGE_PASS:
+                if item.sell_in < 10:
                     self._improve_quality(item)
-                if item.sell_in < 6:
+                if item.sell_in < 5:
                     self._improve_quality(item)
         else:
-            if item.name != ITEM_SULFURAS:
+            if item.name != SULFURAS:
                 self._reduce_quality(item)
-
-    def _update_sell_in_value(self, item):
-        if item.name != ITEM_SULFURAS:
-            item.sell_in = item.sell_in - 1
 
     def _update_quality_when_sellin_passed(self, item):
         if item.sell_in < 0:
-            if item.name != ITEM_AGED_BRIE:
-                if item.name != ITEM_BACKSTAGE_PASS:
-                    if item.name != ITEM_SULFURAS:
+            if item.name != AGED_BRIE:
+                if item.name != BACKSTAGE_PASS:
+                    if item.name != SULFURAS:
                         self._reduce_quality(item)
                 else:
                     item.quality = 0
@@ -60,5 +56,6 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 class Stock_Item(Item):
-    def _test(self):
-        return 1
+    def _update_sell_in_value(self):
+        if self.name != SULFURAS:
+            self.sell_in -= 1
