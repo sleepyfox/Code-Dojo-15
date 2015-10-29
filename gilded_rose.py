a57c8a5a7e9ad1a1fd64e701e39580cce4a141fe
Fox <sleepyfox@gmail.com>
2015-10-29 10:46:08 +0000

    [refactor] Split out quality update methods

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,33 +11,39 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != ITEM_AGED_BRIE and item.name != ITEM_BACKSTAGE_PASS:
-                if item.quality > 0:
-                    if item.name != ITEM_SULFURAS:
-                        item.quality = item.quality - 1
+            self._update_quality(item)
+            if item.name != ITEM_SULFURAS:
+                item.sell_in = item.sell_in - 1
+            self._update_quality_when_sellin_passed(item)
+
+    def _update_quality(self, item):
+        if item.name != ITEM_AGED_BRIE and item.name != ITEM_BACKSTAGE_PASS:
+            if item.quality > 0:
+                if item.name != ITEM_SULFURAS:
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == ITEM_BACKSTAGE_PASS:
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+
+    def _update_quality_when_sellin_passed(self, item):
+        if item.sell_in < 0:
+            if item.name != ITEM_AGED_BRIE:
+                if item.name != ITEM_BACKSTAGE_PASS:
+                    if item.quality > 0:
+                        if item.name != ITEM_SULFURAS:
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == ITEM_BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != ITEM_SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != ITEM_AGED_BRIE:
-                    if item.name != ITEM_BACKSTAGE_PASS:
-                        if item.quality > 0:
-                            if item.name != ITEM_SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
 
 class Item:
