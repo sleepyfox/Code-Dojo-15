d1cc16a978d82f9c3cc8a3d9e6299314adbb837d
Fox <sleepyfox@gmail.com>
2015-10-29 09:28:31 +0000

    [test] added for sell by date decreasing

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,7 +10,11 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals(4, items[0].quality)
 
-    # def test_item_sell_by_decreases_by_one(self):
+    def test_item_sell_by_decreases_by_one(self):
+        items = [Item("Elixir of the Mongoose,", 5, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(4, items[0].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
