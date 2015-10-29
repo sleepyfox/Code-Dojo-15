b0eed823422112bf4a1a4e8226d9ab0e3442af0c
Fox <sleepyfox@gmail.com>
2015-10-29 09:26:28 +0000

    [test] added test for decrease in quality

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,11 +4,13 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_item_decreases_in_quality_by_one(self):
+        items = [Item("Elixir of the Mongoose,", 5, 5)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(4, items[0].quality)
+
+    # def test_item_sell_by_decreases_by_one(self):
 
 if __name__ == '__main__':
     unittest.main()
