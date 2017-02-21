6b30ed975087387b9062760b7d7e0daa80f26470
Nigel Runnels-Moss <sleepyfox@gmail.com>
2017-02-21 11:11:43 +0000

    moved solution to its own folder

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
 
-# Symbolic constants
-AGED_BRIE = "Aged Brie"
-BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
-SULFURAS = "Sulfuras, Hand of Ragnaros"
-TEN_DAYS = 10
-FIVE_DAYS = 5
-MAX_ITEM_QUALITY = 50
-MIN_ITEM_QUALITY = 0
-LEGENDARY_ITEM_QUALITY = 80
-
 class GildedRose(object):
+
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            item.age_one_day()
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.name != "Aged Brie":
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
+                            if item.name != "Sulfuras, Hand of Ragnaros":
+                                item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - item.quality
+                else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -27,48 +45,3 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-class Stock_Item(Item):
-    def out_of_date(self):
-        return self.sell_in < 0
-
-    def age_one_day(self):
-        self.sell_in -= 1
-        self.update_quality()
-
-    def _improve_quality(self, amount):
-        self.quality += amount
-        if self.quality > MAX_ITEM_QUALITY:
-            self.quality = MAX_ITEM_QUALITY
-
-    def _reduce_quality(self, amount):
-        self.quality -= amount
-        if self.quality < MIN_ITEM_QUALITY:
-            self.quality = MIN_ITEM_QUALITY
-
-    def update_quality(self):
-        if self.out_of_date():
-            self._reduce_quality(2)
-        else:
-            self._reduce_quality(1)
-
-class Legendary_Item(Stock_Item):
-    def age_one_day(self):
-        self.quality = LEGENDARY_ITEM_QUALITY
-
-class Cheese_Item(Stock_Item):
-    def update_quality(self):
-        if self.out_of_date():
-            self._improve_quality(2)
-        else:
-            self._improve_quality(1)
-
-class Backstage_Item(Stock_Item):
-    def update_quality(self):
-        if self.out_of_date():
-            self.quality = MIN_ITEM_QUALITY
-        elif self.sell_in < FIVE_DAYS:
-            self._improve_quality(3)
-        elif self.sell_in < TEN_DAYS:
-            self._improve_quality(2)
-        else:
-            self._improve_quality(1)
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -2,22 +2,17 @@
 from __future__ import print_function
 
 from gilded_rose import *
+import csv
 
 if __name__ == "__main__":
     print ("OMGHAI!")
-    items = [
-             Stock_Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Stock_Item(name="Aged Brie", sell_in=2, quality=0),
-             Stock_Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Legendary_Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Legendary_Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Stock_Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Stock_Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Stock_Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Stock_Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
+    items = []
+    with open("items.csv") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            items.append(Item(row['Name'], int(row['sellIn']), int(row['quality'])))
 
-    days = 2
+    days = 1
     import sys
     if len(sys.argv) > 1:
         days = int(sys.argv[1]) + 1
@@ -28,3 +23,4 @@ if __name__ == "__main__":
             print(item)
         print("")
         GildedRose(items).update_quality()
+
